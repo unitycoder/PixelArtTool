2cf7e42e0442a4e9b2ced3ae6f53d6a6417836b0
unitycoder <support@unitycoder.com>
2018-11-24 21:24:11 +0200

    add palette loading from resource, get palette colors to array, paint with palette colors

--- PixelArtTool/MainWindow.xaml.cs
+++ PixelArtTool/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -60,7 +61,78 @@ namespace PixelArtTool
             i.MouseLeftButtonDown += new MouseButtonEventHandler(i_MouseLeftButtonDown);
             i.MouseRightButtonDown += new MouseButtonEventHandler(i_MouseRightButtonDown);
             w.MouseWheel += new MouseWheelEventHandler(w_MouseWheel);
+
+            LoadPalette();
         }
+
+        PixelColor[] palette;
+
+        void LoadPalette()
+        {
+            Uri uri = new Uri("pack://application:,,,/Resources/Palettes/aap-64-1x.png");
+            var img = new BitmapImage(uri);
+
+            // get colors
+            var pixels = GetPixels(img);
+
+            var width = pixels.GetLength(0);
+            var height = pixels.GetLength(1);
+
+            palette = new PixelColor[width * height];
+
+            int index = 0;
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    var c = pixels[x, y];
+                    //                    Console.WriteLine(c.Red + "," + c.Green + "," + c.Blue);
+                    palette[index++] = c;
+                }
+            }
+
+        }
+
+        // https://stackoverflow.com/a/1740553/5452781
+        public unsafe static void CopyPixels2(BitmapSource source, PixelColor[,] pixels, int stride, int offset, bool dummy)
+        //        public unsafe static void CopyPixels(BitmapSource source, PixelColor[,] pixels, int stride, int offset)
+        {
+            fixed (PixelColor* buffer = &pixels[0, 0])
+                source.CopyPixels(
+                  new Int32Rect(0, 0, source.PixelWidth, source.PixelHeight),
+                  (IntPtr)(buffer + offset),
+                  pixels.GetLength(0) * pixels.GetLength(1) * sizeof(PixelColor),
+                  stride);
+        }
+
+        [StructLayout(LayoutKind.Explicit)]
+        public struct PixelColor
+        {
+            // 32 bit BGRA 
+            [FieldOffset(0)] public UInt32 ColorBGRA;
+            // 8 bit components
+            [FieldOffset(0)] public byte Blue;
+            [FieldOffset(1)] public byte Green;
+            [FieldOffset(2)] public byte Red;
+            [FieldOffset(3)] public byte Alpha;
+        }
+
+        public PixelColor[,] GetPixels(BitmapSource source)
+        {
+            if (source.Format != PixelFormats.Bgra32)
+                source = new FormatConvertedBitmap(source, PixelFormats.Bgra32, null, 0);
+
+            int width = source.PixelWidth;
+            int height = source.PixelHeight;
+            PixelColor[,] result = new PixelColor[width, height];
+
+            CopyPixels2(source, result, width * 4, 0, false);
+            //source.CopyPixels(result, width * 4, 0, false);
+            return result;
+        }
+
+        // from palette
+        int currentColorIndex = 0;
 
         // https://docs.microsoft.com/en-us/dotnet/api/system.windows.media.imaging.writeablebitmap?redirectedfrom=MSDN&view=netframework-4.7.2
         // The DrawPixel method updates the WriteableBitmap by using
@@ -87,10 +159,10 @@ namespace PixelArtTool
                     pBackBuffer += y * writeableBitmap.BackBufferStride;
                     pBackBuffer += x * 4;
 
-                    // Compute the pixel's color.
-                    int color_data = 255 << 16; // R
-                    color_data |= 0 << 8;   // G
-                    color_data |= 0 << 0;   // B
+                    // test
+                    currentColorIndex = ++currentColorIndex % palette.Length;
+
+                    int color_data = (int)palette[currentColorIndex].ColorBGRA;
 
                     // Assign the color data to the pixel.
                     *((int*)pBackBuffer) = color_data;
@@ -119,7 +191,7 @@ namespace PixelArtTool
             if (x < 0 || x > resolutionX - 1) return;
             if (y < 0 || y > resolutionY - 1) return;
 
-            Int32Rect rect = new Int32Rect(x,y, 1, 1);
+            Int32Rect rect = new Int32Rect(x, y, 1, 1);
             writeableBitmap.WritePixels(rect, ColorData, 4, 0);
         }
 
